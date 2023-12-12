f5f4f160c96d9b31db44f6ef576d44d4e8065b65
Matt Antonelli <contact@raelys.com>
2023-12-12 15:20:36 -0500

    Enrich metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 
@@ -11,8 +11,17 @@ import Search from "@/components/search";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://github-explorer-ochre.vercel.app/"),
   title: "GitHub Explorer",
-  description: "User profile explorer for GitHub."
+  description: "User profile explorer for GitHub.",
+  openGraph: {
+    title: "GitHub Explorer",
+    description: "User profile explorer for GitHub."
+  }
+};
+
+export const viewport: Viewport = {
+  themeColor: "#1e2327"
 };
 
 export default function RootLayout({
